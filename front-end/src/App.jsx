05ac67de5012a0b8adab4a392162589751bd2aa4
Thúy Hảo <leminhlongit@gmail.com>
2024-05-03 13:50:24 +0700

    detail_product

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState, useEffect } from 'react'
 import HomeComponent from './components/HomeComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import ListProductComponent from './components/ListProductComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProductComponent from './components/ProductComponent'
 function App() {
 
   return (
     <>
-      <HeaderComponent />
-      <ListProductComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<HomeComponent />} />
+          <Route path="/products" element={<ListProductComponent />} />
+          <Route path="/products/:id" element={<ProductComponent/>} />
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
--- front-end/src/services/ProductService.js
+++ front-end/src/services/ProductService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://localhost:8080/products";
+const REST_API_BASE_URL = "http://localhost:8080/sugarnest/v0.1/products";
 
 export const listProducts = (page) => axios.get(`${REST_API_BASE_URL}?page=${page}`);
+export const getProduct = (id) => axios.get(`${REST_API_BASE_URL}/${id}`);
