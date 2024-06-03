ec253f864bb4629d50ae88e4dae9e52d8ec8b686
Thúy Hảo <leminhlongit@gmail.com>
2024-06-03 12:23:53 +0700

    pull_main

--- front-end/src/admin/components/AdminAccountComponent.jsx
+++ front-end/src/admin/components/AdminAccountComponent.jsx
@@ -1,14 +1,188 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import EmlementButtonComponent from './EmlementButtonComponent'
-import AppTitleComponent from './AppTitleComponent'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Space, Badge } from 'antd';
+import { PlusOutlined, EditOutlined, DeleteOutlined, EyeOutlined } from '@ant-design/icons';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import ReusableTableComponent from './ReusableTableComponent';
+import EmlementButtonComponent from './EmlementButtonComponent';
+import AppTitleComponent from './AppTitleComponent';
+import { render } from 'react-dom';
+import { urlImage } from '../../client/services/ProductService';
+import EditModal from '../util/EditModal';
+
 
 const AdminAccountComponent = () => {
     const navigate = useNavigate()
-
     function getRoleManagement() {
         navigate('/admin/account-role-manager')
     }
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:8080/sugarnest/v0.1/account/all')
+            .then(response => {
+                if (response.data.code === 200 && response.data.result.length > 0) {
+                    setData(response.data.result.map(item => ({
+                        key: item.id,
+                        id: item.id,
+                        fullName: item.fullName,
+                        accountName: item.accountName,
+                        birthday: item.birthday,
+                        address: item.address,
+                        email: item.email,
+                        phone: item.phone,
+                        isActive: item.isActive,
+                        createAt: item.createAt,
+                        updateAt: item.updateAt,
+                        roles: item.roles.map(role => ({
+                            name: role.name,
+                            description: role.description,
+                            permissions: role.permissions.map(permission => ({
+                                name: permission.name,
+                                description: permission.description
+                            }))
+                        }))
+                    })));
+                }
+            })
+            .catch(error => {
+                console.error("There was an error fetching the data!", error);
+            });
+    }, []);
+
+    const handleDelete = (key) => {
+        Swal.fire({
+            title: 'Bạn có chắc không?',
+            text: "Bạn sẽ không thể khôi phục điều này!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Vâng, xóa nó đi!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire('Đã xóa!', 'Tài khoản đã bị xóa.', 'success');
+                setData(data.filter(item => item.key !== key));
+            }
+        });
+    };
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [account, setAccount] = useState(null);
+
+    const showModal = async (id) => {
+        try {
+            const response = await axios.get(`http://localhost:8080/sugarnest/v0.1/account/${id}`);
+            if (response.data.code === 200) {
+                setAccount(response.data.result);
+                setIsModalVisible(true);
+            } else {
+                console.error('Failed to fetch data');
+            }
+        } catch (error) {
+            console.error('Error fetching data', error);
+        }
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+        setAccount(null);
+    };
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+            width: '5%',
+            searchable: true,
+            sortable: true,
+        },
+        {
+            title: 'Tên đăng nhập',
+            dataIndex: 'accountName',
+            key: 'accountName',
+            searchable: true,
+            sortable: true,
+        },
+        {
+            title: 'Họ và tên',
+            dataIndex: 'fullName',
+            key: 'fullName',
+            searchable: true,
+            sortable: true,
+        },
+        {
+            title: 'Ngày sinh',
+            dataIndex: 'birthday',
+            key: 'birthday',
+            render: (text) => new Date(text).toLocaleDateString(),
+        },
+        {
+            title: 'Địa chỉ',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+        },
+        {
+            title: 'Số điện thoại',
+            dataIndex: 'phone',
+            key: 'phone',
+        },
+        {
+            title: 'Trạng thái hoạt động',
+            dataIndex: 'isActive',
+            key: 'isActive',
+            render: (text) => (text === 'true' ? 'Hoạt động' : 'Không hoạt động'),
+        },
+        {
+            title: 'Ngày tạo',
+            dataIndex: 'createAt',
+            key: 'createAt',
+            render: (text) => new Date(text).toLocaleString(),
+        },
+        {
+            title: 'Ngày cập nhật',
+            dataIndex: 'updateAt',
+            key: 'updateAt',
+            render: (text) => new Date(text).toLocaleString(),
+        },
+        {
+            title: 'Quyền',
+            dataIndex: 'roles',
+            key: 'roles',
+            render: (roles) => roles.map(role => role.name).join(', '),
+        },
+        {
+            title: 'Chức năng',
+            key: 'action',
+            render: (text, record) => (
+                <Space size="middle">
+                    <Button
+                        type="primary"
+                        className='btn btn-primary btn-sm edit'
+                        icon={<EditOutlined />}
+                        onClick={() => showModal(record.key)}
+                    >
+                        Sửa
+                    </Button>
+                    <Button
+                        type="danger"
+                        className='btn btn-primary btn-sm trash'
+                        icon={<DeleteOutlined />}
+                        onClick={() => handleDelete(record.key)}
+                    >
+                        Xóa
+                    </Button>
+                </Space>
+            ),
+        },
+    ];
 
     return (
         <main className="app-content">
@@ -17,65 +191,19 @@ const AdminAccountComponent = () => {
                 <div className="col-md-12">
                     <div className="tile">
                         <div className="tile-body">
-                            <EmlementButtonComponent/>
-                            <table
-                                className="table table-hover table-bordered js-copytextarea responsive-table"
-                                cellPadding={0}
-                                cellSpacing={0}
-                                border={0}
-                                id="sampleTable" >
-                                <thead>
-                                    <tr>
-                                        <th>ID nhân viên</th>
-                                        <th width={150}>Họ và tên</th>
-                                        <th width={20}>Ảnh thẻ</th>
-                                        <th width={300}>Địa chỉ</th>
-                                        <th>Ngày sinh</th>
-                                        <th>Giới tính</th>
-                                        <th>SĐT</th>
-                                        <th>Chức vụ</th>
-                                        <th width={100}>Tính năng</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>#YUI21376</td>
-                                        <td>Nguyễn Thị Mai</td>
-                                        <td>
-                                            <img
-                                                className="img-card-person"
-                                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-1/425504046_2140968452928397_7020557538431123240_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH2-BFQhORuzFcTYJZzY-5qZg94BacUkQlmD3gFpxSRCcMF0tZoTYWItW45EDmJJKs8Z_lZv6P2nDo-aMOhop4u&_nc_ohc=7zWpDauEJh0Q7kNvgHlvWRV&_nc_ht=scontent.fsgn2-4.fna&oh=00_AYAtCDq2rbM1An_Itflfc48D_4yFDUGDLsyVayR8lA3AfQ&oe=6658DAD4"
-                                                alt=""
-                                            />
-                                        </td>
-                                        <td>Đường Số 3, Tân Tạo A, Bình Tân, Hồ Chí Minh</td>
-                                        <td>09/12/2000</td>
-                                        <td>Nữ </td>
-                                        <td>0836333037</td>
-                                        <td>Tư vấn</td>
-                                        <td>
-                                            <button
-                                                className="btn btn-primary btn-sm trash"
-                                                title="Xóa"
-                                            >
-                                                <i className="fas fa-trash-alt" />
-                                            </button>
-                                            <button
-                                                className="btn btn-primary btn-sm edit"
-                                                title="Sửa"
-                                                id="show-emp"
-                                                data-toggle="modal"
-                                                data-target="#ModalUP"
-                                            >
-                                                <i className="fas fa-edit" />
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <EmlementButtonComponent />
+                            <Button
+                                type="primary"
+                                onClick={getRoleManagement}
+                                style={{ marginBottom: 16 }}
+                            >
+                                Quản lý quyền tài khoản
+                            </Button>
+                            <ReusableTableComponent columns={columns} data={data} />
                         </div>
                     </div>
                 </div>
+                {isModalVisible && account && (<EditModal account={account} onCancel={handleCancel} />)}
             </div>
         </main>
     )
--- front-end/src/admin/util/EditModal.jsx
+++ front-end/src/admin/util/EditModal.jsx
@@ -1,112 +1,107 @@
 import React from 'react'
 
-const EditModal = () => {
+const EditModal = ({ account, onCancel }) => {
   return (
-    <div><div
-    className="modal fade"
-    id="ModalUP"
-    tabIndex={-1}
-    role="dialog"
-    aria-hidden="true"
-    data-backdrop="static"
-    data-keyboard="false"
-  >
-    <div className="modal-dialog modal-dialog-centered" role="document">
-      <div className="modal-content">
-        <div className="modal-body">
-          <div className="row">
-            <div className="form-group  col-md-12">
-              <span className="thong-tin-thanh-toan">
-                <h5>Chỉnh sửa thông tin nhân viên cơ bản</h5>
-              </span>
+    <div
+      className="modal fade show"
+      id="ModalUP"
+      tabIndex={-1}
+      role="dialog"
+      aria-hidden="true"
+      style={{ display: 'block' }} // Hiển thị modal
+      data-backdrop="static"
+      data-keyboard="false"
+    >
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          <div className="modal-body">
+            <div className="row">
+              <div className="form-group  col-md-12">
+                <span className="thong-tin-thanh-toan">
+                  <h5>Chỉnh sửa tài khoản</h5>
+                </span>
+              </div>
             </div>
+            <div className="row">
+              <div className="form-group col-md-6">
+                <label className="control-label">ID nhân viên</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  required=""
+                  defaultValue={account.id}
+                  disabled=""
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label className="control-label">Tên tài khoản</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  required=""
+                  defaultValue={account.accountName}
+                  disabled=""
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label className="control-label">Họ và tên</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  required=""
+                  defaultValue={account.fullName}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label className="control-label">Số điện thoại</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  required=""
+                  defaultValue={account.phone}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label className="control-label">Địa chỉ email</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  required=""
+                  defaultValue={account.email}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label className="control-label">Ngày sinh</label>
+                <input
+                  className="form-control"
+                  type="date"
+                  defaultValue={account.birthday.split('T')[0]}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label className="control-label">Địa chỉ</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  required=""
+                  defaultValue={account.address}
+                />
+              </div>
+            </div>
+            <br />
+            <a className="btn btn-save" type="button">
+              Lưu lại
+            </a>
+            <a style={{marginLeft:'10px'}} className="btn btn-cancel" data-dismiss="modal" onClick={onCancel}>
+              Hủy bỏ
+            </a>
+            <br />
+            <br />
           </div>
-          <div className="row">
-            <div className="form-group col-md-6">
-              <label className="control-label">ID nhân viên</label>
-              <input
-                className="form-control"
-                type="text"
-                required=""
-                defaultValue="#CD2187"
-                disabled=""
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label className="control-label">Họ và tên</label>
-              <input
-                className="form-control"
-                type="text"
-                required=""
-                defaultValue="Võ Trường"
-              />
-            </div>
-            <div className="form-group  col-md-6">
-              <label className="control-label">Số điện thoại</label>
-              <input
-                className="form-control"
-                type="number"
-                required=""
-                defaultValue="033 666 8888"
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label className="control-label">Địa chỉ email</label>
-              <input
-                className="form-control"
-                type="text"
-                required=""
-                defaultValue="truong.vd2000@gmail.com"
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label className="control-label">Ngày sinh</label>
-              <input
-                className="form-control"
-                type="date"
-                defaultValue="15/03/2000"
-              />
-            </div>
-            <div className="form-group  col-md-6">
-              <label htmlFor="exampleSelect1" className="control-label">
-                Chức vụ
-              </label>
-              <select className="form-control" id="exampleSelect1">
-                <option>Bán hàng</option>
-                <option>Tư vấn</option>
-                <option>Dịch vụ</option>
-                <option>Thu Ngân</option>
-                <option>Quản kho</option>
-                <option>Bảo trì</option>
-                <option>Kiểm hàng</option>
-                <option>Bảo vệ</option>
-                <option>Tạp vụ</option>
-              </select>
-            </div>
-          </div>
-          <br />
-          <a
-            href="#"
-            style={{ float: "right", fontWeight: 600, color: "#ea0000" }}
-          >
-            Chỉnh sửa nâng cao
-          </a>
-          <br />
-          <br />
-          <button className="btn btn-save" type="button">
-            Lưu lại
-          </button>
-          <a className="btn btn-cancel" data-dismiss="modal" href="#">
-            Hủy bỏ
-          </a>
-          <br />
         </div>
-        <div className="modal-footer"></div>
       </div>
     </div>
-  </div>
-  </div>
-  )
-}
+  );
+};
 
 export default EditModal
