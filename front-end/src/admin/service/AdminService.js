26b1544c2203b908d3c83ccb3f125802ff02e4bd
Thúy Hảo <leminhlongit@gmail.com>
2024-06-13 22:43:59 +0700

    update

--- front-end/src/admin/service/AdminService.js
+++ front-end/src/admin/service/AdminService.js
@@ -5,16 +5,18 @@ export const REST_API_BASE_URL = "http://localhost:8080/sugarnest/v0.1";
 
 export const loginToken = (accountName, password) => axios.post(`${REST_API_BASE_URL}/auth/login`, { accountName, password });
 export const hasPermission = (requiredPermission) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    
-    if (user || !user.roles) {
-        const roles = user.roles;
-        for (let role of roles) {
-          if (role.permissions.some(permission => permission.name === requiredPermission)) {
-            return true;
-          }
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user) {
+    if (user.roles) {
+      const roles = user.roles;
+      for (let role of roles) {
+        if (role.permissions.some(permission => permission.name === requiredPermission)) {
+          return true;
         }
-      
-        return false;
+      }
+
+      return false;
     }
-  };
+  }
+};
